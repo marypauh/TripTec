42d23eec1da2e1238cdb647535c6e29ebdf1932c
Castillo666 <kevcastilloh@gmail.com>
2018-11-15 10:26:02 -0600

    ke ase

--- mavenproject1/src/main/java/Estructura/Nodo.java
+++ mavenproject1/src/main/java/Estructura/Nodo.java
@@ -5,10 +5,25 @@
  */
 package Estructura;
 
-/**
- *
- * @author kevca
- */
-public class Nodo {
-    
+public class Nodo{
+
+    public int vertice;        //Almacena en dato del vértice
+    public int peso;        //Almacena el peso de la arista
+
+    //Constructor de nodo vacío
+    public Nodo(){
+        this.vertice = 0;
+        this.peso = 0;
+    }//fin del constructor Nodo
+
+
+    //Constructor de nodo con el dato del vértice destino y el peso de la arista
+    public Nodo(int v, int p){
+        this.vertice = v;
+        this.peso = p;
+    }
+
+    public String aCadena(){
+        return "|" + this.vertice + "|" + this.peso + "|->";
+    }//fin método aCadena
 }
--- mavenproject1/src/main/java/Estructura/NodoVertice.java
+++ mavenproject1/src/main/java/Estructura/NodoVertice.java
@@ -5,10 +5,27 @@
  */
 package Estructura;
 
-/**
- *
- * @author kevca
- */
-public class NodoVertice {
-    
+import java.util.*;
+
+public class NodoVertice{
+    public int vertice;                    //Almacena el dato del vértice
+    public boolean visitado;            //Indica si el vértice ha sido visitado o no
+    public LinkedList <Nodo> aristas;    //Almacena la lista de aristas del vértice
+
+    public NodoVertice(){
+        this.vertice = 0;
+        this.visitado = false;
+        this.aristas  = new LinkedList<Nodo>();
+    }//fin del constructor Nodo
+
+    public NodoVertice(int v){
+            this.vertice = v;
+            this.visitado = false;
+            this.aristas  = new LinkedList<Nodo>();
+    }//fin del constructor Nodo
+
+    public String aCadena(){
+        return "|" + this.vertice + "|" + "|->";
+    }//fin método aCadena
+
 }
