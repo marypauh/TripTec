595b6679b78c44f2190cb72e813460efcffa370b
marypauh <maripau8@gmail.com>
2018-11-24 11:57:22 -0600

    interfaz

--- mavenproject1/src/main/java/Main/Main.java
+++ mavenproject1/src/main/java/Main/Main.java
@@ -5,13 +5,17 @@ import com.google.gson.GsonBuilder;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
+import Interface.Menu;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyCuI-6vEJkDHlpzgwMsHKDP9mXoVqaTEn8").build();
-        GeocodingResult[] results =  GeocodingApi.geocode(context,"1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        System.out.println(gson.toJson(results[0].addressComponents));
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        
+        //GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyCuI-6vEJkDHlpzgwMsHKDP9mXoVqaTEn8").build();
+        //GeocodingResult[] results =  GeocodingApi.geocode(context,"1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+        //Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        //System.out.println(gson.toJson(results[0].addressComponents));
     }
 }
