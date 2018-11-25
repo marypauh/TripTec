c95aa37564f51d0197ba44e7d09eb7c5552a2157
marypauh <maripau8@gmail.com>
2018-11-25 04:07:55 -0600

    getter, setter

--- mavenproject1/src/main/java/Structures/Clients.java
+++ mavenproject1/src/main/java/Structures/Clients.java
@@ -20,4 +20,54 @@ public class Clients {
     this.birthDate = birthDate;
     this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Graph getWishList() {
+        return wishList;
+    }
+
+    public void setWishList(Graph wishList) {
+        this.wishList = wishList;
+    }
+    
+    
 }
